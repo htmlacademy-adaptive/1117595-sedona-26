ca617f3e8e9187626646496727b7014349f5e8a3
Dmitry <lysogordmitry@gmail.com>
2022-08-29 17:35:47 +0300

    вроде как доделал меню и начал работу со скриптом

---
+++ source/js/script.js
@@ -0,0 +1,8 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-header__nav-toggle');
+
+
+
+navToggle.onclick = function {
+  navToggle.classList.toggle('main-header__nav-toggle--open');
+};
