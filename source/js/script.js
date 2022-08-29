9c492d5ee8e482218395848a21c621c08bb790bd
Dmitry <lysogordmitry@gmail.com>
2022-08-29 22:01:37 +0300

    анимирую выпадающее меню

--- source/js/script.js
+++ source/js/script.js
@@ -3,6 +3,15 @@ let navToggle = document.querySelector('.main-header__nav-toggle');
 
 
 
-navToggle.onclick = function {
+navToggle.addEventListener('click', function() {
   navToggle.classList.toggle('main-header__nav-toggle--open');
-};
+
+  if (navMain.classList.contains('main-nav--close')) {
+    navMain.classList.remove('main-nav--close')
+    navMain.classList.add('main-nav--open')
+
+} else {
+  navMain.classList.add('main-nav--close')
+  navMain.classList.remove('main-nav--open')
+  }
+})
